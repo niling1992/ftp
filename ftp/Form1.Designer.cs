34bae78422ba2094b0304358adc3eed5c5b01c7d
niling <lin_ni@shyms.org>
2016-09-07 15:32:35 +0800

    2016/9/7

---
+++ ftp/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ftp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_title = new System.Windows.Forms.TextBox();
+            this.textBox_content = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.button_attachment = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_attachment = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_title
+            // 
+            this.textBox_title.AcceptsReturn = true;
+            this.textBox_title.AcceptsTab = true;
+            this.textBox_title.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_title.Location = new System.Drawing.Point(93, 14);
+            this.textBox_title.Name = "textBox_title";
+            this.textBox_title.Size = new System.Drawing.Size(341, 29);
+            this.textBox_title.TabIndex = 0;
+            // 
+            // textBox_content
+            // 
+            this.textBox_content.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_content.Location = new System.Drawing.Point(93, 57);
+            this.textBox_content.Multiline = true;
+            this.textBox_content.Name = "textBox_content";
+            this.textBox_content.Size = new System.Drawing.Size(341, 139);
+            this.textBox_content.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(32, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "主题";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(32, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "正文";
+            // 
+            // button_submit
+            // 
+            this.button_submit.Location = new System.Drawing.Point(211, 232);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(87, 34);
+            this.button_submit.TabIndex = 4;
+            this.button_submit.Text = "提交";
+            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_attachment
+            // 
+            this.button_attachment.Location = new System.Drawing.Point(12, 201);
+            this.button_attachment.Name = "button_attachment";
+            this.button_attachment.Size = new System.Drawing.Size(68, 26);
+            this.button_attachment.TabIndex = 5;
+            this.button_attachment.Text = "添加附件";
+            this.button_attachment.UseVisualStyleBackColor = true;
+            this.button_attachment.Click += new System.EventHandler(this.button_attachment_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox_attachment
+            // 
+            this.textBox_attachment.BackColor = System.Drawing.Color.White;
+            this.textBox_attachment.Location = new System.Drawing.Point(93, 205);
+            this.textBox_attachment.Name = "textBox_attachment";
+            this.textBox_attachment.ReadOnly = true;
+            this.textBox_attachment.Size = new System.Drawing.Size(340, 21);
+            this.textBox_attachment.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 278);
+            this.Controls.Add(this.textBox_attachment);
+            this.Controls.Add(this.button_attachment);
+            this.Controls.Add(this.button_submit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_content);
+            this.Controls.Add(this.textBox_title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_title;
+        private System.Windows.Forms.TextBox textBox_content;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.Button button_attachment;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox_attachment;
+    }
+}
+
---
+++ ftp/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace ftp
+{
+    public partial class Form1 : Form
+    {
+        bool isAttExisted = false;
+        string attachment;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Download(string filePath, string fileSrc, string fileName, string ftpServerIP, string ftpUserName, string ftpPwd)
+        {
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+            using (FileStream OutputStream = new FileStream(filePath + "\\" + fileName, FileMode.Create))
+            {
+                FtpWebRequest ReqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + ftpServerIP + "/" + fileSrc));
+
+                ReqFTP.Method = WebRequestMethods.Ftp.DownloadFile;
+
+                ReqFTP.UseBinary = true;
+
+                ReqFTP.Credentials = new NetworkCredential(ftpUserName, ftpPwd);
+
+                using (FtpWebResponse response = (FtpWebResponse)ReqFTP.GetResponse())
+                {
+
+                    using (Stream FtpStream = response.GetResponseStream())
+                    {
+
+                        long Cl = response.ContentLength;
+
+                        int bufferSize = 2048;
+
+                        int readCount;
+
+                        byte[] buffer = new byte[bufferSize];
+
+                        readCount = FtpStream.Read(buffer, 0, bufferSize);
+
+                        while (readCount > 0)
+                        {
+                            OutputStream.Write(buffer, 0, readCount);
+
+                            readCount = FtpStream.Read(buffer, 0, bufferSize);
+                        }
+
+                        FtpStream.Close();
+                    }
+
+                    response.Close();
+
+                }
+
+                OutputStream.Close();
+            }
+
+        }
+        private void UploadSmall(string sFileDstPath, string FolderName, string ftpServerIP, string ftpUserName, string ftpPwd)
+        {
+
+            FileInfo fileInf = new FileInfo(sFileDstPath);
+
+            FtpWebRequest reqFTP;
+
+            reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + ftpServerIP + "/" + FolderName + "/" + fileInf.Name));
+
+            reqFTP.Credentials = new NetworkCredential(ftpUserName, ftpPwd);
+
+            reqFTP.KeepAlive = false;
+
+            reqFTP.Method = WebRequestMethods.Ftp.UploadFile;
+
+            reqFTP.UseBinary = true;
+
+            reqFTP.ContentLength = fileInf.Length;
+
+            int buffLength = 2048;
+
+            byte[] buff = new byte[buffLength];
+
+            int contentLen;
+
+            using (FileStream fs = fileInf.OpenRead())
+            {
+
+                using (Stream strm = reqFTP.GetRequestStream())
+                {
+
+                    contentLen = fs.Read(buff, 0, buffLength);
+
+                    while (contentLen != 0)
+                    {
+
+                        strm.Write(buff, 0, contentLen);
+
+                        contentLen = fs.Read(buff, 0, buffLength);
+                    }
+
+                    strm.Close();
+                }
+
+                fs.Close();
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw;
+            string sub = textBox_title.Text;
+            string filename = sub + ".txt";
+            string content = textBox_content.Text;
+            if (!File.Exists(filename))
+            {
+                //不存在就新建一个文本文件,并写入一些内容
+                sw = File.CreateText(filename);
+                sw.Write(content);
+            }
+            else
+            {
+                //如果存在就添加一些文本内容
+                sw = File.AppendText(filename);
+                sw.Write(content);
+            }
+            sw.Close();
+            UploadSmall(".\\" + filename, "UPLOAD", "210.21.48.7:2312", "FSYMSXT", "FSYMSXT");
+            if (isAttExisted)
+                UploadSmall(attachment, "UPLOAD", "210.21.48.7:2312", "FSYMSXT", "FSYMSXT");
+            MessageBox.Show("提交成功，谢谢您的意见反馈！");
+        }
+
+        private void button_attachment_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                isAttExisted = true;
+                attachment = openFileDialog1.FileName;
+                textBox_attachment.Text = attachment;
+            }
+            else
+                return;
+        }
+        
+    }
+}
